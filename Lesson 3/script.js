3afed949a0597cc3124b699b86fad75026b1a8fd
Andre Almeida <andre-silva78@hotmail.com>
2021-03-07 16:07:01 -0300

    build: lesson 03 basic scene creation

--- Lesson 3/script.js
+++ Lesson 3/script.js
@@ -1,1 +1,12 @@
+// Scene
 const scene = new THREE.Scene();
+
+// Red Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1); //Geometry has 3 unit/parameter
+const material = new THREE.MeshBasicMaterial({ color: 'red' }); //U need to provide a object to pass parameters
+const mesh = new THREE.Mesh(geometry, material); // Mesh is a compose of geometry and material
+scene.add(mesh); // Add component to scene
+
+// Camera
+const camera = new THREE.PerspectiveCamera(); // First parameter: Field of View on Vertical
+scene.add(camera); // Add Camera to Scene
